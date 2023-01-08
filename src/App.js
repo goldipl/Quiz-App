628df5e31e9657ed58c7359cfb5adb0fb8b82d26
goldipl <mgodlewski92@gmail.com>
2023-01-08 13:24:58 +0100

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import TitleContainer from "./componenets/TitleContainer";
 
 const App = () => {
+
+  const [quiz, setQuiz] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://localhost:8000/quiz");
+      const json = response.json();
+      setQuiz(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[])
+
   return (
     <div className="app_container">
       <TitleContainer />
